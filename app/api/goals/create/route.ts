993ad3321ca8c8f38e773584bd604d8cecbd9aeb
cocoyuy <xy2141@nyu.edu>
2025-04-29 22:25:00 -0400

    Add user auth check

--- app/api/goals/create/route.ts
+++ app/api/goals/create/route.ts
@@ -5,12 +5,12 @@ import { NextResponse } from 'next/server';
 import { headers } from "next/headers"
 
 export async function POST(req: Request) {
-    // const session = await auth.api.getSession({
-    //     headers: await headers()
-    // })
-    // if (!session || !session.user?.id) {
-    //     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-    // }
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session || !session.user?.id) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
 
     const body = await req.json();
     const { title, description, difficulty, timeEstimate } = body;
